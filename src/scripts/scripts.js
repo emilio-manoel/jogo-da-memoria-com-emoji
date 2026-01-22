0aedb1652ec797eb75e46be20bf822aa814c5202
emilio-manoel <emilio.trabalho555@gmail.com>
2026-01-22 12:02:20 -0300

    adicionando emojis no js

---
+++ src/scripts/scripts.js
@@ -0,0 +1,14 @@
+const emojis = [
+    "😎", "😎", "🐱‍👤", "🐱‍👤", "🕵️‍♀️", "🕵️‍♀️", "🎓", "🎓", "🏅", "🏅", "📱", "📱", "🍰", "🍰", "✈", "✈"
+];
+let openCards = [];
+
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for( let i=0; i < emojis.length; i++){
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    document.querySelector(".game").appendChild(box);
+}
